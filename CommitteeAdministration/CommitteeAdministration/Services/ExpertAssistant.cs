9f1720920efb66f1037a27adfd1dff330e3df545
vahid asbaghi <vahid.asbaghi@hotmail.com>
2017-10-05 07:19:08 +0330

    add expertassistant

---
+++ CommitteeAdministration/CommitteeAdministration/Services/ExpertAssistant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CommitteeAdministration.Helper;
+using CommitteeManagement.Model;
+using CommitteeManagement.Repository;
+using Microsoft.Practices.Unity;
+
+namespace CommitteeAdministration.Services
+{
+    public class ExpertAssistant
+    {
+        private readonly IMainContainer _mainContainer = ModelContainer.Instance.Resolve<IMainContainer>();
+        public void AlarmExpert(User user)
+        {
+            //var committee=
+        }
+    }
+}
